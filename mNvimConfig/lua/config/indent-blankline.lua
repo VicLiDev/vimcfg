5703d7d576dc5b0c48b22c9574d035ee1659ad65
Hongjin Li <vic.hong@rock-chips.com>
2024-04-18 17:21:34 +0800

    nvim: update for indent-blankline

--- mNvimConfig/lua/config/indent-blankline.lua
+++ mNvimConfig/lua/config/indent-blankline.lua
@@ -1,15 +1,7 @@
 local api = vim.api
 
 local exclude_ft = { "help", "git", "markdown", "snippets", "text", "gitconfig", "alpha" }
-require("ibl").setup {
-  -- U+2502 may also be a good choice, it will be on the middle of cursor.
-  -- U+250A is also a good choice
-  char = "▏",
-  show_end_of_line = false,
-  disable_with_nolist = true,
-  buftype_exclude = { "terminal" },
-  filetype_exclude = exclude_ft,
-}
+require("ibl").setup()
 
 local gid = api.nvim_create_augroup("indent_blankline", { clear = true })
 api.nvim_create_autocmd("InsertEnter", {

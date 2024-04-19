b06b3139dd06a6e1e77f0be398fa1626c8ea188f
Hongjin Li <vic.hong@rock-chips.com>
2024-04-19 11:37:44 +0800

    nvim: disable indent-blankline some func

--- mNvimConfig/lua/config/indent-blankline.lua
+++ mNvimConfig/lua/config/indent-blankline.lua
@@ -4,18 +4,18 @@ local exclude_ft = { "help", "git", "markdown", "snippets", "text", "gitconfig",
 require("ibl").setup()
 
 local gid = api.nvim_create_augroup("indent_blankline", { clear = true })
-api.nvim_create_autocmd("InsertEnter", {
-  pattern = "*",
-  group = gid,
-  command = "IndentBlanklineDisable",
-})
-
-api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  group = gid,
-  callback = function()
-    if not vim.tbl_contains(exclude_ft, vim.bo.filetype) then
-      vim.cmd([[IndentBlanklineEnable]])
-    end
-  end,
-})
+-- api.nvim_create_autocmd("InsertEnter", {
+--   pattern = "*",
+--   group = gid,
+--   command = "IndentBlanklineDisable",
+-- })
+-- 
+-- api.nvim_create_autocmd("InsertLeave", {
+--   pattern = "*",
+--   group = gid,
+--   callback = function()
+--     if not vim.tbl_contains(exclude_ft, vim.bo.filetype) then
+--       vim.cmd([[IndentBlanklineEnable]])
+--     end
+--   end,
+-- })
